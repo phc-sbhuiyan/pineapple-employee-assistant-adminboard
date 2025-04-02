5ecd87bab5d47fcd513d76587113cc6c53dd2dfe
Sharif Bhuiyan <106225926+phc-sbhuiyan@users.noreply.github.com>
2025-04-02 12:34:45 -0700

    Update pineapple_bot.py

--- pineapple_bot.py
+++ pineapple_bot.py
@@ -1,68 +1,26 @@
 import streamlit as st
 import os
-from langchain.chains import LLMChain
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.memory import ConversationBufferMemory
-from trulens_eval import TruChain, Feedback, OpenAI, Tru
+from trulens.core import TruSession
+from base import rag # a rag app with a query method
+from base import tru_rag # a rag app wrapped by trulens
+from trulens.dashboard import streamlit as trulens_st
+from trulens.dashboard.Leaderboard import render_leaderboard
 
-openai = OpenAI()
-tru = Tru()
-tru.reset_database()
+session = TruSession()
 
-# Load environment variables
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-#os.environ["HUGGINGFACE_API_KEY"] = st.secrets["HUGGINGFACE_API_KEY"]
+def generate_and_log_response(input_text):
+    with tru_rag as recording:
+        response = rag.query(input_text)
+    record = recording.get()
+    return record, response
 
-# Build LLM chain
-template = """You are a chatbot having a conversation with a human.
-        {chat_history}
-        Human: {human_input}
-        Chatbot:"""
-prompt = PromptTemplate(
-    input_variables=["chat_history", "human_input"], template=template
-)
-memory = ConversationBufferMemory(memory_key="chat_history")
-llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
+with st.form("my_form"):
+    text = st.text_area("Enter text:", "How do I launch a streamlit app?")
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        record, response = generate_and_log_response(text)
+        st.info(response)
+        trulens_st.trulens_feedback(record=record)
+        trulens_st.trulens_trace(record=record)
 
-# Question/answer relevance between overall question and answer.
-f_relevance = Feedback(openai.relevance).on_input_output()
-
-# Moderation metrics on output
-f_hate = Feedback(openai.moderation_hate).on_output()
-f_violent = Feedback(openai.moderation_violence, higher_is_better=False).on_output()
-f_selfharm = Feedback(openai.moderation_selfharm, higher_is_better=False).on_output()
-f_maliciousness = Feedback(openai.maliciousness_with_cot_reasons, higher_is_better=False).on_output()
-
-# TruLens Eval chain recorder
-chain_recorder = TruChain(
-    chain, app_id="contextual-chatbot", feedbacks=[f_relevance, f_hate, f_violent, f_selfharm, f_maliciousness]
-)
-
-# Streamlit frontend
-st.title("Contextual Chatbot")
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        # Record with TruLens
-        with chain_recorder as recording:
-            full_response = chain.run(prompt)
-        message_placeholder = st.empty()
-        message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append(
-        {"role": "assistant", "content": full_response})
-
-tru.run_dashboard()
+render_leaderboard()
